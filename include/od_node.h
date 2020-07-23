e0275b187fc1542bc8c620d614feccdcff6cae0c
Ray Redondo <rdrpenguin04@gmail.com>
2020-07-23 13:31:53 -0500

    Change dealloc/destruct again

--- include/od_node.h
+++ include/od_node.h
@@ -11,8 +11,8 @@ typedef struct od_node_iter od_node_iter;
 typedef struct od_node_iter_vtable od_node_iter_vtable;
 
 struct od_node_iter_vtable {
-    void (*dealloc_data)(od_node_iter*);
-    void (*destruct_data)(od_node_iter*);
+    void (*dealloc_data)(od_node);
+    void (*destruct_data)(od_node);
     od_node* (*next)(od_node_iter *iter);
     void *reserved3;
     void *reserved4;
@@ -27,8 +27,8 @@ struct od_node_iter {
 };
 
 struct od_node_vtable {
-    void (*dealloc_data)(od_node*);
-    void (*destruct_data)(od_node*);
+    void (*dealloc)(od_node);
+    void (*destruct)(od_node);
     od_node_iter* (*get_child_iterator)(od_node *parent);
     const char* (*get_uuid)(od_node*);
     void *reserved4;
@@ -41,6 +41,12 @@ struct od_node {
     od_node_vtable *vtable;
     void *data;
 };
+
+void destroy_node_iter_data(od_node_iter*);
+void free_node_iter_data(od_node_iter*);
+
+void destroy_node_data(od_node*);
+void free_node_data(od_node*);
 
 #ifdef __cplusplus
 }
