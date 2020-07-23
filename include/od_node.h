ecf707d40c2c292b8d6210689662dfd8e35087a4
Ray Redondo <rdrpenguin04@gmail.com>
2020-07-23 00:46:23 -0500

    Sane iterator

--- include/od_node.h
+++ include/od_node.h
@@ -7,13 +7,28 @@ extern "C" {
 
 typedef struct od_node od_node;
 typedef struct od_node_vtable od_node_vtable;
+typedef struct od_node_iter od_node_iter;
+typedef struct od_node_iter_vtable od_node_iter_vtable;
 
-// Call the iterator to get the next node
-typedef od_node* od_node_iter(od_node *parent);
+struct od_node_iter_vtable {
+    void (*dealloc)(od_node_iter*);
+    void (*destroy)(od_node_iter*);
+    od_node* (*next)(od_node_iter *iter);
+    void *reserved3;
+    void *reserved4;
+    void *reserved5;
+    void *reserved6;
+    void *reserved7;
+};
+
+struct od_node_iter {
+    od_node_iter_vtable *vtable;
+    // Extensible struct
+};
 
 struct od_node_vtable {
-    void (*dealloc)(od_node *parent);
-    void (*destroy)(od_node *parent);
+    void (*dealloc)(od_node*);
+    void (*destroy)(od_node*);
     od_node_iter* (*get_child_iterator)(od_node *parent);
     void *reserved3;
     void *reserved4;
