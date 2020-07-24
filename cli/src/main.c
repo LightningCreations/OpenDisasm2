5f84d1e607ebd0ca9161476651bf08a73721cc91
Ray Redondo <rdrpenguin04@gmail.com>
2020-07-24 15:38:23 -0500

    Broken

--- cli/src/main.c
+++ cli/src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <od_module.h>
+
+const int NUM_MODULES = 1;
+
+const char *modules[NUM_MODULES] = {
+    "disasm_elf"
+};
 
 int main(int argc, char **argv) {
     if(argc < 2) {
         printf("Usage: %s <file>\n", argv[0]);
         return 1;
+    }
+    for(int i = 0; i < NUM_MODULES; i++) {
+        od_load_module(modules[i]);
     }
 }
