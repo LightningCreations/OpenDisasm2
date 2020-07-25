37fecd24c59aa8c0b99cb4f7d6a2afeb0e6593f8
Ray Redondo <rdrpenguin04@gmail.com>
2020-07-24 21:16:10 -0500

    Segfault, as expected.

--- libdisasm/src/od_module.c
+++ libdisasm/src/od_module.c
@@ -16,8 +16,30 @@ void od_load_module(char* name) {
 
 #else // Mac and Linux
 
+// Extra includes
+#include <dlfcn.h>
+
 void od_load_module(char* name) {
-    // WIP
+    char *libname = malloc(
+            3 + // "lib"
+            strlen(name) +
+#ifdef __APPLE__
+            6 + // ".dylib"
+#else // most POSIX
+            3 + // ".so"
+#endif
+            1); // "\0"
+    libname[0] = 0;
+    strcat(libname, "lib");
+    strcat(libname, name);
+#ifdef __APPLE__
+    strcat(libname, ".dylib");
+#else
+    strcat(libname, ".so");
+#endif
+    void *module = dlopen(libname, RTLD_LAZY | RTLD_LOCAL);
+    od_bool (*od_module_register)(od_module_register_fn*, void*) = dlsym(module, "od_module_register");
+    od_module_register(od_module_register_fn_def, NULL); // base will be replaced later with something useful
 }
 
 #endif
