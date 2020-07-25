dd96c318ee0a5272c9ce4cc73af2ea992169d1ba
Ray Redondo <rdrpenguin04@gmail.com>
2020-07-24 23:31:24 -0500

    Improve soundness

--- libdisasm/include/od_module.h
+++ libdisasm/include/od_module.h
@@ -13,13 +13,13 @@ extern "C" {
 //   * disasm_elf.dll on Windows
 //   * libdisasm_elf.dylib on Mac OS X
 //   * libdisasm_elf.so on Linux
-void od_load_module(od_data*, char *name);
+void od_load_module(od_data*, const char *name);
 
 // Call to detect if a block of data can be disassembled by this disassembler
 typedef od_bool od_detect_fn(od_data*, od_node_raw *data);
 
 // Called by modules to register critical base functions
-typedef void od_module_register_fn(od_data*, char *uuid, od_detect_fn *detect_fn);
+typedef void od_module_register_fn(od_data*, const char *uuid, od_detect_fn *detect_fn);
 
 // Defined by modules, called by the loader.
 od_bool od_module_register(od_data*, od_module_register_fn *register_fn);
